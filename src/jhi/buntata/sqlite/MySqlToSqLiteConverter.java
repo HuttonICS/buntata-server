bfdbdf0b51d2254d52a07d0799c839ec108fab6f
sebastian.raubach <sebastian.raubach@hutton.ac.uk>
2016-11-25 13:41:31 +0000

    Merge branch 'master' of https://github.com/buntata/buntata-server
    # Conflicts:
    #	README.md
    
    - CHG: Updated copyright information.

--- src/jhi/buntata/sqlite/MySqlToSqLiteConverter.java
+++ src/jhi/buntata/sqlite/MySqlToSqLiteConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Information & Computational Sciences, The James Hutton Institute
+ * Copyright 2016 Information & Computational Sciences, The James Hutton Institute
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
